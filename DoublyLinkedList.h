7e67061973454d9c3e13bf5ca07f1931c9984993
ppovali <ppovalikhin@mail.ru>
2025-10-24 23:17:51 +0300

    Add push_back function

--- DoublyLinkedList.h
+++ DoublyLinkedList.h
@@ -13,7 +13,26 @@ public:
     DoubleLinkedList() : head(nullptr), taill(nullptr), size(0) {}
     ~DoubleLinkedList() {clear();}
 
-    void push_back()
+    void push_back(const T& value) {
+        Node<T>* nd = new Node<T>(value);
+        if (!tail) head = tail = nd;
+        else {
+            tail->next = nd;
+            nd->prev = tail;
+            tail - nd;
+        }
+        ++size;
+    }
+    void push_back(T&& value) {
+        Node<T>* nd = new Node<t>(std::move(value));
+        if (!tail) head = tail = nd;
+        else {
+            tail->next = nd;
+            nd->prev = tail;
+            tail = nd;
+        }
+        ++size;
+    }
 
 };
 
